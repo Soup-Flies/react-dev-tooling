075dfe14fdf4012951fdd9076ac89a135e58ef1f
Ben Charais <benjcharais@gmail.com>
2018-08-08 22:31:05 -0600

    working client

--- src/index.js
+++ src/index.js
@@ -2,47 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './containers/App';
+import './styles.css';
 
-if (process.env.NODE_ENV !== 'production') console.log('this is dev mode');
-else console.log('this is prod mode');
-
-/* eslint-disable */
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(registration => {
-        console.log('SW registered: ', registration);
-      })
-      .catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
+if (process.env.NODE_ENV !== 'production') console.log('Development is live on the client');
+else {
+  // Dont have cache busting for development in yet
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log('SW registered: ', registration);
+        })
+        .catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
+        });
+    });
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-// import './styles.css';
-// import _ from 'lodash';
-
-// function component() {
-//   var element = document.createElement('div');
-
-//   var button = document.createElement('button');
-//   var br = document.createElement('br');
-
-//   button.innerHTML = 'Click me and look at the console!';
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.appendChild(br);
-//   element.appendChild(button);
-
-//   button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-//     var print = module.default;
-
-//     print();
-//   })
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
--- src/print.js
+++
@@ -1,5 +0,0 @@
-console.log("this print.js module has loaded!")
-
-export default () => {
-  console.log("button clicked: printed stuff");
-}
